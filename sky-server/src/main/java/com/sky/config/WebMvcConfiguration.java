e654ac971f2ab66abb3adb1ea5165adccd14ab06
seven <seven@email.com>
2025-04-20 22:24:38 +0800

    添加购物车

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -45,8 +45,8 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
                 .excludePathPatterns("/admin/employee/login");
 
         registry.addInterceptor(jwtTokenUserInterceptor)
-                .addPathPatterns("user/**")
-                .excludePathPatterns("user/user/login")
+                .addPathPatterns("/user/**")
+                .excludePathPatterns("/user/user/login")
                 .excludePathPatterns("/user/shop/status");
     }
 
